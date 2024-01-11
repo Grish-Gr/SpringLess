831aa3a2a2dc9d25c741e72c6ce45d88534259ae
Grish-Gr <maks_ter23@mail.ru>
2024-01-12 00:46:15 +0500

    Skill_box 1 (1.7 задание)

---
+++ skill_box_1/src/main/java/maks/ter/app/services/ProjectRepository.java
@@ -0,0 +1,11 @@
+package maks.ter.app.services;
+
+import java.util.List;
+
+public interface ProjectRepository<T> {
+    List<T> retreiveAll();
+
+    void store(T book);
+
+    boolean removeItemById(Integer bookIdToRemove);
+}
